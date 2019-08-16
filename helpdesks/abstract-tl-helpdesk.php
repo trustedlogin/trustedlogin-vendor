0179d9b7d23e14e2539772f08f324dd2dd136e41
Zack Katz <zack@katz.co>
2019-08-16 14:45:52 -0600

    Only add settings fields when the help desk is active

--- helpdesks/abstract-tl-helpdesk.php
+++ helpdesks/abstract-tl-helpdesk.php
@@ -44,12 +44,24 @@ abstract class HelpDesk {
 
 		$this->debug_mode = $this->tls_settings_is_toggled( 'tls_debug_enabled' );
 
+		add_filter( 'trustedlogin_supported_helpdesks', array( $this, 'add_supported_helpdesk' ) );
+
+		if ( ! $this->is_active() ) {
+			return;
+		}
+
 		add_action( 'admin_init', array( $this, 'add_extra_settings' ) );
 
 		add_action( 'wp_ajax_' . static::slug . '_webhook', array( $this, 'webhook_endpoint' ) );
 		add_action( 'wp_ajax_nopriv_' . static::slug . '_webhook', array( $this, 'webhook_endpoint' ) );
 
-		add_filter( 'trustedlogin_supported_helpdesks', array( $this, 'add_supported_helpdesk' ) );
+	}
+
+	public function is_active() {
+
+		$active_helpdesk = $this->tls_settings_get_selected_helpdesk();
+
+		return in_array( static::slug, $active_helpdesk, true );
 	}
 
 	public function add_supported_helpdesk( $helpdesks = array() ) {
