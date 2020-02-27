d8290e58ee1a463cc57aeac27b9af3c8957484f8
Hector Kolonas <me@inztinkt.com>
2020-02-27 16:25:50 -0500

    add unit testing for TrustedLogin_Encryption class

---
+++ tests/test-encryption.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Class EncryptionTest
+ *
+ * @package Tl_Support_Side
+ */
+
+/**
+ * Tests for Audit Logging
+ */
+class EncryptionTest extends WP_UnitTestCase {
+
+	/** @var TrustedLogin_Support_Side */
+	private $TL;
+	
+	private $encryption;
+
+	
+	/**
+	 * AuditLogTest constructor.
+	 */
+	public function __construct() {
+		$this->TL = new TrustedLogin_Support_Side;
+		$this->TL->setup();
+		
+		$this->encryption = new TrustedLogin_Encryption();
+	}
+
+	/**
+	 * @covers TrustedLogin_Encryption::create_keys
+	 */
+	function test_create_keys() {
+
+		$keys = $this->invokeMethod( $this->encryption, 'create_keys' );
+
+		$this->assertIsObject( $keys ), 'create_keys should return an object' );
+		$this->assertObjectHasAttribute( 'public_key', $keys, 'public_key should be returned by create_keys ');
+		$this->assertObjectHasAttribute( 'private_key', $keys, 'private_key should be returned by create_keys ');
+
+	}
+
+	/**
+	 * @covers TrustedLogin_Encryption::get_keys
+	 */
+	function test_get_keys() {
+
+		$keys = $this->invokeMethod( $this->encryption, 'get_keys' );
+
+		$this->assertIsObject( $keys ), 'get_keys should return an object' );
+		$this->assertObjectHasAttribute( 'public_key', $keys, 'public_key should be returned by get_keys ');
+		$this->assertObjectHasAttribute( 'private_key', $keys, 'private_key should be returned by get_keys ');
+
+	}
+
+	/**
+	 * @covers TrustedLogin_Encryption::get_public_key
+	 */
+	function test_get_public_key() {
+
+		$public_key = $this->encryption::get_public_key();
+
+		$this->assertIsString( $public_key )
+	}
+
+	/**
+	 * @covers TrustedLogin_Encryption::create_identity_nonce
+	 */
+	function test_create_identity_nonce() {
+
+		$nonces = $this->encryption::create_identity_nonce();
+
+		$this->assertIsArray( $nonces, 'create_identity_nonce should return an array' );
+
+		$this->assertArrayHasKey( 'nonce', $nonces, 'create_identity_nonce return array should contain a nonce key' );
+		$this->assertArrayHasKey( 'signed', $nonces, 'create_identity_nonce return array should contain a signed key' );
+
+	}
+
+	/**
+	 * Tests to make sure the public key can be used to decrypt the nonce
+	 */
+	function test_decrypt_nonce(){
+
+		$nonces = $this->encryption::create_identity_nonce();
+
+		$keys = $this->invokeMethod( $this->encryption, 'get_keys' );
+
+		openssl_public_decrypt( base64_decode( $nonces['signed'] ), $decrypted, $keys->public_key, OPENSSL_PKCS1_PADDING);
+
+		$this->assertIsString( $decrypted, 'openssl_public_decrypt should return a string' );
+
+		$nonce_decoded = base64_decode( $nonces['nonce'] );
+
+		$this->assertIsString( $nonce_decoded, 'base64_decode( nonces[nonce] ) should return a string' );
+
+		$this->assertIsEqual( $decrypted, $nonce_decoded, 'decrypting nonces[signed] should equal nonces[nonce]' );
+	
+	}
+
+	
+	
+	
+}
