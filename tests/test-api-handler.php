31c4ece5df6fc34a618924bdb96cc6045e8231f4
Zack Katz <zack@katz.co>
2020-02-27 16:37:22 -0700

    Remove Vault API tests

--- tests/test-api-handler.php
+++ tests/test-api-handler.php
@@ -36,11 +36,5 @@ class APIHandlerTest extends WP_UnitTestCase {
 
 		$this->assertEquals( 'Authorization', $saas_api_handler->get_auth_header_type() );
 
-		$vault_api_handler = new TL_API_Handler( 'type=vault' );
-
-		$this->assertEquals( 'https://vault.trustedlogin.com/' . $vault_api_handler::vault_api_version . '/', $vault_api_handler->get_api_url() );
-
-		$this->assertEquals( 'X-Vault-Token', $vault_api_handler->get_auth_header_type() );
-
 	}
 }
