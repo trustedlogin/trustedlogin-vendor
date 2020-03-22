1c31fd31dd181bb7b86a7c9111f254c6bf425d94
Zack Katz <zack@katz.co>
2020-03-22 17:52:43 -0600

    Namespace main plugin file

--- trustedlogin-vendor.php
+++ trustedlogin-vendor.php
@@ -12,6 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Copyright: © 2020 Katz Web Services, Inc.
  */
+namespace TrustedLogin\Vendor;
 
 if (!defined('ABSPATH')) {
     exit;
@@ -32,7 +33,7 @@ require_once $path . 'includes/class-tl-api-handler.php';
 require_once $path . 'includes/class-trustedlogin-audit-log.php';
 require_once $path . 'includes/class-trustedlogin-encryption.php';
 
-class TrustedLogin_Support_Side {
+class Plugin {
 
 	use \TrustedLogin\Vendor\Debug_Logging;
 	use \TrustedLogin\Vendor\Licensing;
@@ -53,12 +54,13 @@ class TrustedLogin_Support_Side {
 	 */
 	private $settings;
 
-	public function __construct() {}
+	public function __construct() {
+		$this->plugin_version = TRUSTEDLOGIN_PLUGIN_VERSION;
+	}
 
 	public function setup() {
 		global $wpdb;
 
-		$this->plugin_version = TRUSTEDLOGIN_PLUGIN_VERSION;
 
 		/*
 		 * Filter allows site admins to override SSL check on dev/testing servers.
@@ -72,11 +74,11 @@ class TrustedLogin_Support_Side {
 			return false;
 		}
 
-		$this->settings = new TrustedLogin\Vendor\Settings();
+		$this->settings = new Settings();
 
-		$this->endpoint = new TrustedLogin\Vendor\Endpoint( $this->settings );
+		$this->endpoint = new Endpoint( $this->settings );
 
-		add_action( 'init', array( $this, 'init_helpdesk_integration' ) );
+		$this->load_helpdesks();
 	}
 
 	/*
@@ -86,12 +88,12 @@ class TrustedLogin_Support_Side {
 	 */
 	public function ssl_admin_notice() {
 	    $class = 'notice notice-error';
-	    $message = __( 'TrustedLogin plugin NOT enabled. SSL required to securely interact with TrustedLogin servers.', 'trustedlogin' );
+	    $message = __( 'The TrustedLogin plugin is NOT enabled. SSL is required to securely interact with TrustedLogin servers.', 'trustedlogin-vendor' );
 
 	    printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
 
-	public function init_helpdesk_integration() {
+	public function load_helpdesks() {
 
 		include_once plugin_dir_path( __FILE__ ) . 'helpdesks/abstract-tl-helpdesk.php';
 
@@ -103,17 +105,13 @@ class TrustedLogin_Support_Side {
 
 }
 
-function init_tl_vendor(){
-
-	$init_tl = new TrustedLogin_Support_Side();
+add_action( 'plugins_loaded', function() {
+	$init_tl = new Plugin();
 	$init_tl->setup();
+} );
 
-}
+register_deactivation_hook( __FILE__, 'trustedlogin_vendor_deactivate' );
 
-add_action( 'after_setup_theme', 'init_tl_vendor' );
-
-register_deactivation_hook( __FILE__, 'trustedlogin_supportside_deactivate' );
-
-function trustedlogin_supportside_deactivate() {
+function trustedlogin_vendor_deactivate() {
     delete_option('tl_permalinks_flushed');
 }
