3ba536ae2a9934f828dfca7821a02b54774daa69
Zack Katz <zack@katz.co>
2020-03-22 17:59:45 -0600

    Merge new HS class with old

--- helpdesks/class-tl-helpscout.php
+++ helpdesks/class-tl-helpscout.php
@@ -6,8 +6,10 @@ namespace TrustedLogin\Vendor;
  *
  * @package tl-support-side
  * @version 0.1.0
- **/
+ */
 class HelpScout extends HelpDesk {
+
+    use Debug_Logging;
 
 	const name = 'Help Scout';
 
@@ -17,181 +19,351 @@ class HelpScout extends HelpDesk {
 
 	const is_active = true;
 
-	private $debug_mode = true;
+    /**
+     * @var string The secret to verify requests from HelpScout
+     * @since 0.1.0
+     **/
+    private $secret;
 
-	private $secret = '';
+    /**
+     * @var boolean Whether our debug logging is activated
+     * @since 0.1.0
+     **/
+    private $debug_mode;
 
-	public function __construct() {
-		parent::__construct();
+    /**
+     * @var array Current TrustedLogin settings
+     * @since 0.1.0
+     **/
+    private $options;
 
-		$this->secret = '';//$this->tls_settings_get_value('tls_' . self::slug . '_secret');
-	}
+    /**
+     * @var array Default TrustedLogin settings
+     * @since 0.1.0
+     **/
+    private $default_options;
 
-	public function add_extra_settings() {
+    /**
+     * @var stdClass This helpdesk's settings
+     * @since 0.1.0
+     **/
+    private $details;
 
-		add_settings_field(
-			'tls_' . self::slug . '_secret',
-			self::name . ' ' . __( 'Secret Key', 'trustedlogin-vendor' ),
-			array( $this, 'secret_field_render' ),
-			'TLS_plugin_options',
-			'tls_options_section'
-		);
+    public function __construct() {
 
-		add_settings_field(
-			'tls_' . self::slug . '_url',
-			self::name . ' ' . __( 'Webhook URL', 'trustedlogin-vendor' ),
-			array( $this, 'url_field_render' ),
-			'TLS_plugin_options',
-			'tls_options_section'
-		);
+	    $this->details = (object) array(
+		    'name'    => __( 'Help Scout', 'tl-support-side' ),
+		    'slug'    => 'helpscout',
+		    'version' => '0.1.0',
+	    );
 
-	}
+	    $this->settings   = new Settings();
+	    $this->secret     = $this->settings->get_setting( self::slug . '_secret' );
+	    $this->debug_mode = $this->settings->debug_mode_enabled();
 
-	public function secret_field_render()
-    {
+	    add_action( 'admin_init', array( $this, 'add_extra_settings' ) );
 
-        $this->tls_settings_render_input_field('tls_' . self::slug . '_secret', 'password', false);
+	    add_action( 'wp_ajax_' . self::slug . '_webhook', array( $this, 'webhook_endpoint' ) );
+	    add_action( 'wp_ajax_nopriv_' . self::slug . '_webhook', array( $this, 'webhook_endpoint' ) );
 
     }
 
-    public function url_field_render()
-    {
+    /**
+     * Checks that the secret/api_key for helpscout is set in settings panel.
+     *
+     * @since 0.1.0
+     *
+     * @return bool  Whether the secret is set and not empty.
+     */
+    public function has_secret(){
+        if ( ! isset( $this->secret ) || empty( $this->secret )) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Appends extra settings into the TrustedLogin plugin settings page.
+     *
+     * @since 0.1.0
+     */
+    public function add_extra_settings() {
+
+	    $settings = new Settings();
+
+	    if ( self::slug === $settings->get_setting( 'helpdesk' ) ) {
+
+		    add_settings_field(
+			    'trustedlogin_vendor_' . self::slug . '_secret',
+			    self::name . ' ' . __( 'Secret Key', 'tl-support-side' ),
+			    array( $this, 'secret_field_render' ),
+			    'trustedlogin_vendor_options',
+			    'trustedlogin_vendor_options_section'
+		    );
+
+		    add_settings_field(
+			    'trustedlogin_vendor_' . self::slug . '_url',
+			    sprintf( __( '%s Callback URL', 'tl-support-side' ), self::name ),
+			    array( $this, 'url_field_render' ),
+			    'trustedlogin_vendor_options',
+			    'trustedlogin_vendor_options_section'
+		    );
+
+	    }
+    }
+
+    /**
+     * Renders the settings field for the helpdesk secret/api_key
+     */
+    public function secret_field_render( $field ){
+        $this->settings->render_input_field(self::slug . '_secret', 'password', false);
+    }
+
+    /**
+     * Renders the settings field for the helpdesk url
+     */
+    public function url_field_render(){
+
         $url = add_query_arg('action', self::slug . '_webhook', admin_url('admin-ajax.php'));
 
-        echo '<input readonly="readonly" type="text" value="' . $url . '" class="regular-text widefat">';
+        echo '<input readonly="readonly" type="text" value="' . esc_url( $url ) . '" class="regular-text widefat code">';
+
     }
 
-    public function webhook_endpoint() {
+    /**
+     * Generates the output for the helpscout widget.
+     *
+     * Checks the `$_SERVER` array for the signature and verifies the source before checking for licenses matching to users email.
+     *
+     * @uses self::helpscout_verify_source()
+     *
+     * @since 0.1.0
+     * @since 0.9.2 - added the status of licenses to output
+     *
+     * @return a JSON response back to an Ajax request.
+     */
+    public function webhook_endpoint(){
 
-        $signature = (isset($_SERVER['X-HELPSCOUT-SIGNATURE'])) ? $_SERVER['X-HELPSCOUT-SIGNATURE'] : null;
-        $data = file_get_contents('php://input');
+	    $signature = null;
 
-        if (!$this->helpscout_verify_source($data, $signature)) {
-        	#wp_send_json_error(array('message' => 'Unauthorized'), 401);
+	    if ( isset( $_SERVER['X-HELPSCOUT-SIGNATURE'] ) ) {
+		    $signature = $_SERVER['X-HELPSCOUT-SIGNATURE'];
+	    } elseif ( isset( $_SERVER['HTTP_X_HELPSCOUT_SIGNATURE'] ) ) {
+		    $signature = $_SERVER['HTTP_X_HELPSCOUT_SIGNATURE'];
+	    } elseif ( function_exists( 'apache_request_headers' ) ) {
+		    $headers = apache_request_headers();
+		    if ( isset( $headers['X-HelpScout-Signature'] ) ) {
+			    $signature = $headers['X-HelpScout-Signature'];
+		    }
+	    }
+
+        $data = file_get_contents( 'php://input' );
+
+        if ( ! $this->helpscout_verify_source( $data, $signature ) ) {
+            wp_send_json( array(
+            	'html' => '<p class="red">Unauthorized.</p><p>Verify your site\'s TrustedLogin Settings match the Help Scout widget settings.</p>'
+            ), 401 );
         }
 
         $licenses = array();
+        $data_obj = json_decode( $data, false );
+        $email    = sanitize_email( $data_obj->customer->email );
 
-        $this->dlog("data: $data", __METHOD__);
+        if ( false === ( $licenses = get_transient( 'trustedlogin_licenses_'.md5( $email ) ) ) ){
 
-        $data_obj = json_decode($data, false);
+            if ( $this->is_edd_store() && ! empty( $email ) ) {
 
-        $email = sanitize_email($data_obj->customer->email);
+                if ( $this->has_edd_licensing() ) {
+                    $licenses = $this->edd_get_licenses( $email );
+                }
 
-	    $email = 'zack@gravityview.co';
+            }
 
-	    if ( empty( $email ) ) {
-		    wp_send_json_error(array('message' => 'Unauthorized'), 401);
-	    }
+            if ( $licenses ){
+                set_transient( 'trustedlogin_licenses_'.md5( $email ), $licenses, DAY_IN_SECONDS );
+            }
 
-        $saas_auth = $this->tls_settings_get_value('tls_account_key');
-
-        if ( ! $saas_auth ) {
-	        $error = __( 'Please make sure the TrustedLogin API Key setting is entered.', 'trustedlogin-vendor' );
-	        $this->dlog( $error, __METHOD__ );
-	        wp_send_json_error( array( 'message' => $error ) );
         }
 
+        /**
+         * Filter: allow for other addons to generate the licenses array
+         *
+         * @since 0.6.0
+         * @param array $licenses [
+         *   @var  object  $license [
+         *     @var  string  status  The status of the license.
+         *     @var  string  key     The license key.
+         *   ]
+         * ]
+         * @param string $email
+         * @return array
+         **/
+        $licenses = apply_filters( 'trusted_login_get_licenses', $licenses, $email );
 
-        $saas_attr = array('type' => 'saas', 'auth' => $saas_auth, 'debug_mode' => $this->debug_mode);
-        $saas_api = new \TL_API_Handler($saas_attr);
+        $account_id = $this->settings->get_setting( 'account_id' );
+        $saas_auth  = $this->settings->get_setting( 'account_key' );
+        $public_key = $this->settings->get_setting( 'public_key' );
 
-	    // Get licenses
-	    $license_generator = License_Generators::get_active();
+        if ( ! $saas_auth || ! $public_key ) {
+            $error = __( 'Please make sure the TrustedLogin API Key setting is entered.', 'tl-support-side' );
+            $this->dlog( $error, __METHOD__ );
+            wp_send_json_error( array( 'message' => $error ) );
+        }
 
-	    $licenses = $license_generator->get_license_keys_by_email( $email );
-
-	    /**
-	     * Filter: allow for other addons to generate the licenses array
-	     *
-	     * @since 0.6.0
-	     * @param array $licenses (
-	     *   @see $response from saas_api to /Sites/?accessKey=<accessKey>
-	     * )
-	     * @param string $email
-	     * @return Array
-	     **/
-	    $licenses = apply_filters('trusted_login_get_licenses', $licenses, $email);
+        $saas_attr = (object) array( 'type' => 'saas', 'auth' => $saas_auth, 'debug_mode' => $this->debug_mode );
+        $saas_api = new TL_API_Handler($saas_attr);
 
         $for_vault = array();
         $item_html = '';
 
         $html_template = '<ul class="c-sb-list c-sb-list--two-line">%1$s</ul>';
-        $item_template = '<li class="c-sb-list-item"><a href="%1$s">%2$s %3$s</a></li>';
+        $item_template = '<li class="c-sb-list-item"><a href="%1$s">%2$s %3$s</a> (%4$s)</li>';
         $no_items_template = '<li class="c-sb-list-item">%1$s</li>';
+        $url_endpoint = apply_filters( 'trustedlogin_redirect_endpoint', 'trustedlogin' );
+
+        $endpoint = 'accounts/' . $account_id . '/sites/';
+        $method   = 'GET';
+        $data     = array( 'accessKeys' => array() );
+
+        $statuses = array();
 
         foreach ( $licenses as $license ) {
 
-            // check licenses for TrustedLogin Sites via SaaS app.
+            $data['accessKeys'][] = $license->key;
+            $statuses[ $license->key ] = $license->status;
 
-            $endpoint = 'Sites/?accessKey=' . $license;
-            $method = 'GET';
-            $body = null;
+        } // foreach($licenses)
+
+        if ( ! empty( $data['accessKeys']) ){
 
             /**
              * Expected result
              *
              * @var $response [
-             *   String $keyStoreID - the id of the secret in Key Store
-             *   String $siteURL - the site the secret was generated on
-             *   String $accountNamespace - the namepsace of the Vendor in TL SaaS
-             *   String $deleteKey - the token to use to Revoke Site from SaaS
+             *   "<license_key>" => [ <secrets> ]
              * ]
              **/
-            $response = $saas_api->api_prepare($endpoint, $data, $method);
+            $response = $saas_api->call( $endpoint, $data, $method );
 
-            $this->dlog("Response: " . print_r($response, true), __METHOD__);
+            $this->dlog( "Response: " . print_r( $response, true ), __METHOD__ );
 
-            if ($response) {
-                if (isset($response->keyStoreID) && isset($response->accountNamespace)) {
-                    $for_vault[] = (array) $response;
+            if ( ! empty( $response ) ){
+                foreach ( $response as $key => $secrets ){
+                    foreach ( $secrets as $secret ){
+                         $item_html .= sprintf(
+                            $item_template,
+                            esc_url( site_url( '/' . $url_endpoint . '/' . $secret ) ),
+                            __( 'TrustedLogin for ', 'tl-support-side' ),
+                            $key,
+                            $statuses[ $key ]
+                        );
+                    }
                 }
             }
 
-        } // foreach($licenses)
+            $this->dlog( "item_html: ". $item_html, __METHOD__ );
 
-        if (!empty($for_vault)) {
-            foreach ($for_vault as $vault_set) {
-                $item_html .= sprintf(
-                    $item_template,
-                    esc_url(site_url('/' . Endpoint::redirect_endpoint . '/' . $vault_set['keyStoreID'])),
-                    __('TrustedLogin to', 'trustedlogin-vendor'),
-                    esc_url($vault_set['siteURL'])
-                );
-            }
+        } else {
+
+            $this->dlog( "No accessKeys found. ", __METHOD__ );
+
         }
 
-        if (empty($item_html)) {
+        if ( empty ( $item_html ) ) {
             $item_html = sprintf(
                 $no_items_template,
-                __('No TrustedLogin sessions authorized for this user.', 'trustedlogin-vendor')
+                __( 'No TrustedLogin sessions authorized for this user.', 'tl-support-side' )
             );
         }
 
-        $return_html = sprintf($html_template, $item_html);
+        $return_html = sprintf( $html_template, $item_html );
 
-        wp_send_json_success(array('html' => $return_html));
+        wp_send_json( array('html' => $return_html ), 200 );
+
     }
 
+    /**
+     * Checks if Easy Digital Downloads Licensing is enabled.
+     *
+     * @since 0.1.0
+     *
+     * @return bool  Whether the `edd_software_licensing` function exists.
+     */
+    public function has_edd_licensing(){
+        return function_exists('edd_software_licensing');
+    }
 
-    public function helpscout_verify_source($data, $signature)
-    {
-        if (!$this->has_secret() || is_null($signature)) {
-            return false;
+    /**
+     * Gets any EDD licenses attached to an email address
+     *
+     * @since 0.1.0
+     *
+     * @param  string  $email  The email to check for EDD licenses
+     *
+     * @return EDD_SL_License[]|false  Array of licenses or false if none are found.
+     **/
+    public function edd_get_licenses( $email ) {
+
+        $licenses = array();
+        $user     = get_user_by( 'email', $email );
+
+        if ( $user ) {
+
+            $licenses = edd_software_licensing()->get_license_keys_of_user( $user->ID, 0, 'all', true );
+
+            foreach ( $licenses as $license ) {
+                $children = edd_software_licensing()->get_child_licenses( $license->ID );
+                if ( $children ) {
+                    foreach ( $children as $child ) {
+                        $licenses[] = edd_software_licensing()->get_license( $child->ID );
+                    }
+                }
+
+                $licenses[] = edd_software_licensing()->get_license( $license->ID );
+            }
         }
 
-        $calculated = base64_encode(hash_hmac('sha1', $data, $this->secret, true));
-        return $signature == $calculated;
+        return ( ! empty( $licenses ) ) ? $licenses : false;
     }
 
-	public function has_secret() {
+    /**
+     * Verifies the source of the Widget AJAX request is from helpscout
+     *
+     * @since 0.1.0
+     *
+     * @param  string  $data provided via `PHP://input`.
+     * @param  string  $signature provided via `$_SERVER` attribute.
+     *
+     * @return bool  if the calculated hash matches the signature provided.
+     */
+    public function helpscout_verify_source( $data, $signature ) {
 
-		if ( ! isset( $this->secret ) || empty( $this->secret ) ) {
-			return false;
-		}
+	    if ( ! $this->has_secret() ) {
+		    $this->dlog( 'No secret is set.', __METHOD__ );
+		    return false;
+	    }
 
-		return true;
-	}
+	    if( is_null( $signature ) ) {
+		    $this->dlog( 'No signature provided. Here is the $_SERVER output' . print_r( $_SERVER, true ), __METHOD__ );
+		    return false;
+	    }
+
+	    $calculated = base64_encode( hash_hmac( 'sha1', $data, $this->secret, true ) );
+
+	    return $signature == $calculated;
+    }
+
+    /**
+     * Checks if the current site is an EDD store
+     *
+     * @since 0.2.0
+     * @return Boolean
+     **/
+    public function is_edd_store()
+    {
+	    return function_exists( 'edd' );
+    }
 
 }
 
