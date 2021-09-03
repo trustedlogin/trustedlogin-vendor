9a722031e9e2e7e55347d84497c72c9f5be128f5
Zack Katz <zack@katz.co>
2021-09-03 15:08:54 -0400

    Start preventing access during 3rd-party plugin security warnings
    #56

---
+++ includes/class-trustedlogin-integrations.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Class: TrustedLogin Settings
+ *
+ * @package trustedlogin-vendor
+ * @version 0.2.0
+ */
+
+namespace TrustedLogin\Vendor;
+
+
+/**
+ * Class Integrations
+ *
+ * In the future, we may break this out into per-integration classes.
+ *
+ * @package TrustedLogin\Vendor
+ */
+class Integrations {
+
+	// WordFence
+	// https://wordpress.org/plugins/wordfence/
+
+	function wordfence() {
+		// Is current IP address blocked?
+	}
+
+	function scururi() {
+
+	}
+
+	// Sucuri
+	// https://wordpress.org/plugins/sucuri-scanner/
+
+	// iThemes Security
+	// https://wordpress.org/plugins/better-wp-security/
+
+	// All in One WP Security
+	// https://wordpress.org/plugins/all-in-one-wp-security-and-firewall/
+
+	// WP Scan Security
+	// https://wordpress.org/plugins/wpscan/
+
+}
